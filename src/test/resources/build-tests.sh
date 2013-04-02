a443d1a7894a31c1e857e9a1f4c08756f85fd18f
Meno Abels <meno.abels@adviser.com>
2013-04-02 22:05:48 +0200

    make the shell script more robust to run on more general conditions

--- src/test/resources/build-tests.sh
+++ src/test/resources/build-tests.sh
@@ -1,68 +1,74 @@
 #!/bin/sh
 
+
 cd $(dirname $0)
+base=$(pwd)
+echo $base
+cd $base
+
+mkdir -p $base/local-m2
 
 cd managed-dependencies/
-mvn -Dmaven.repo.local=../local-m2 clean install || exit 1
+mvn -Dmaven.repo.local=$base/local-m2 clean install || exit 1
 cd ..
 
 cd omit-duplicate/
 cd level3/
-mvn -Dmaven.repo.local=../../local-m2 clean install || exit 1
+mvn -Dmaven.repo.local=$base/local-m2 clean install || exit 1
 cd ..
 cd level2/
-mvn -Dmaven.repo.local=../../local-m2 clean install || exit 1
+mvn -Dmaven.repo.local=$base/local-m2 clean install || exit 1
 cd ..
 cd level1/
-mvn -Dmaven.repo.local=../../local-m2 clean install || exit 1
+mvn -Dmaven.repo.local=$base/local-m2 clean install || exit 1
 cd ..
 cd base/
-mvn -Dmaven.repo.local=../../local-m2 clean install || exit 1
+mvn -Dmaven.repo.local=$base/local-m2 clean install || exit 1
 cd ..
 cd ..
 
 cd property-resolution/
-mvn -Dmaven.repo.local=../local-m2 clean install || exit 1
+mvn -Dmaven.repo.local=$base/local-m2 clean install || exit 1
 cd ..
 
 cd dependency-exclusion/
-mvn -Dmaven.repo.local=../local-m2 clean install || exit 1
+mvn -Dmaven.repo.local=$base/local-m2 clean install || exit 1
 cd ..
 
 cd optional-dependencies/
-mvn -Dmaven.repo.local=../local-m2 clean install || exit 1
+mvn -Dmaven.repo.local=$base/local-m2 clean install || exit 1
 cd ..
 
 cd dependency-exclusion2/
 cd lib/
-mvn -Dmaven.repo.local=../../local-m2 clean install || exit 1
+mvn -Dmaven.repo.local=$base/local-m2 clean install || exit 1
 cd ..
 cd dep1/
-mvn -Dmaven.repo.local=../../local-m2 clean install || exit 1
+mvn -Dmaven.repo.local=$base/local-m2 clean install || exit 1
 cd ..
 cd dep2/
-mvn -Dmaven.repo.local=../../local-m2 clean install || exit 1
+mvn -Dmaven.repo.local=$base/local-m2 clean install || exit 1
 cd ..
 cd base/
-mvn -Dmaven.repo.local=../../local-m2 clean install || exit 1
+mvn -Dmaven.repo.local=$base/local-m2 clean install || exit 1
 cd ..
 cd ..
 
 cd optional-dependencies2/
 cd lib-v1/
-mvn -Dmaven.repo.local=../../local-m2 clean install || exit 1
+mvn -Dmaven.repo.local=$base/local-m2 clean install || exit 1
 cd ..
 cd lib-v2/
-mvn -Dmaven.repo.local=../../local-m2 clean install || exit 1
+mvn -Dmaven.repo.local=$base/local-m2 clean install || exit 1
 cd ..
 cd dep1/
-mvn -Dmaven.repo.local=../../local-m2 clean install || exit 1
+mvn -Dmaven.repo.local=$base/local-m2 clean install || exit 1
 cd ..
 cd dep2/
-mvn -Dmaven.repo.local=../../local-m2 clean install || exit 1
+mvn -Dmaven.repo.local=$base/local-m2 clean install || exit 1
 cd ..
 cd base/
-mvn -Dmaven.repo.local=../../local-m2 clean install || exit 1
+mvn -Dmaven.repo.local=$base/local-m2 clean install || exit 1
 cd ..
 cd ..
 
